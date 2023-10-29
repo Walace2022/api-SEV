bc74b2a7e4a488daf97283730bed180af69ecc13
Henrique Walace <Henriquewalace@hotmail.com>
2023-10-29 15:10:46 -0300

    Adicionado criptografia na senha de Funcionario

--- src/models/Funcionarios.js
+++ src/models/Funcionarios.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const FuncionarioSchema = mongoose.Schema({
   nome: { type: String, required: true },
   CPF: { type: String, required: true, unique: true },
   senha: { type: String, required: true, select: false },
+});
+
+FuncionarioSchema.pre("save", async function (next) {
+  this.senha = await bcrypt.hash(this.senha, 10);
+  next();
 });
 
 const Funcionario = mongoose.model("Funcionario", FuncionarioSchema);
